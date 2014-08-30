c1409bbbfc8c7ecec42f4ae2dacca1adbb22d4a3
rickerliang <rickerliang@gmail.com>
2014-08-30 13:02:17 +0800

    111111

--- timescape_image_cutter/timescape_image_cutter/main.cpp
+++ timescape_image_cutter/timescape_image_cutter/main.cpp
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-	tbb::task_scheduler_init init(tbb::task_scheduler_init::deferred);
-	init.initialize(/*tbb::task_scheduler_init::automatic*/1);
+	tbb::task_scheduler_init init(tbb::task_scheduler_init::automatic);
+	//init.initialize(1);
 	QApplication a(argc, argv);
 	timescape_image_cutter w;
 	w.show();
 	a.exec();
-	init.terminate();
+	//init.terminate();
 	return 0;
 }
