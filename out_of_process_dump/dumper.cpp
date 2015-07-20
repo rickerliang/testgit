210a8cdd386b705e33283d742d21b70029e1a5e7
rickerliang <rickerliang@gmail.com>
2015-07-20 19:19:02 +0800

    update

--- out_of_process_dump/dumper.cpp
+++ out_of_process_dump/dumper.cpp
@@ -4,11 +4,16 @@
 
 #include <memory>
 
+#include <QDir>
 #include <QList>
 #include <QFileInfo>
+#include <QLibrary>
 #include <Psapi.h>
+#include <winternl.h>
+#include <ntstatus.h>
 
 #include "xmlreport.h"
+#include <kso/appfeature/kappfeature.h>
 
 namespace
 {
@@ -48,7 +53,7 @@ minidump基础上加上堆内存
 MINIDUMP_TYPE DumpTypeFull = 
 	static_cast<MINIDUMP_TYPE>(DumpTypeMini | MiniDumpWithFullMemory);
 
-bool createReportXml(
+bool CreateReportXml(
 	HANDLE targetProcess, PEXCEPTION_POINTERS p, 
 	const QList<MINIDUMP_MODULE_CALLBACK>* modules, const QString& dir)
 {
@@ -95,12 +100,11 @@ bool createReportXml(
 	return XmlReport::createInformationLog(
 		dir, exceptionRecord, info, processor, os, memory, moduleList);
 }
-}
 
-BOOL WINAPI Dumper::miniDumpCallback(
-	__inout PVOID CallbackParam,
-	__in    PMINIDUMP_CALLBACK_INPUT CallbackInput,
-	__inout PMINIDUMP_CALLBACK_OUTPUT CallbackOutput
+BOOL __stdcall MiniDumpCallback(
+	PVOID CallbackParam,
+	PMINIDUMP_CALLBACK_INPUT CallbackInput,
+	PMINIDUMP_CALLBACK_OUTPUT CallbackOutput
 	)
 {
 	if (ModuleCallback == CallbackInput->CallbackType)
@@ -115,7 +119,7 @@ BOOL WINAPI Dumper::miniDumpCallback(
 	return TRUE;
 }
 
-QString Dumper::createDumpDir()
+QString CreateDumpDir()
 {
 	const size_t tempDirLen = 320;
 	std::unique_ptr<wchar_t[]> temp(new wchar_t[tempDirLen]);
@@ -135,7 +139,7 @@ QString Dumper::createDumpDir()
 	return directory;
 }
 
-QString Dumper::getProcessName(HANDLE process)
+QString GetProcessName(HANDLE process)
 {
 	const size_t nameLen = 512;
 	std::unique_ptr<wchar_t[]> name(new wchar_t[nameLen]);
@@ -145,7 +149,7 @@ QString Dumper::getProcessName(HANDLE process)
 	return QString::fromWCharArray(name.get());
 }
 
-HANDLE Dumper::createDumpFile(const QString& processName, const QString& dumpDir)
+HANDLE CreateDumpFile(const QString& processName, const QString& dumpDir)
 {
 	QString fileName(processName);
 	fileName += "Dump.dmp";
@@ -165,25 +169,319 @@ HANDLE Dumper::createDumpFile(const QString& processName, const QString& dumpDir
 	return dumpFile;
 }
 
+
+
+bool GetArgValue(const char* arg, const char* argKey, QString* outArgValue)
+{
+	if (arg == nullptr || argKey == nullptr || outArgValue == nullptr)
+	{
+		return false;
+	}
+
+	QString s(arg);
+	auto pos = s.indexOf(argKey);
+	if (pos != -1)
+	{
+		*outArgValue = s.mid(pos + QString(argKey).length());
+		return true;
+	}
+
+	return false;
+}
+
+bool ParseDumpeeArg(int argc, char* argv[], DumpeeArg* outDumpeeArg)
+{
+	if (argc < MandatoryArgCount)
+	{
+		return false;
+	}
+
+	if (outDumpeeArg == nullptr)
+	{
+		return false;
+	}
+
+	int parseResult = 0;
+	DWORD pid = -1;
+	DWORD tid = -1;
+	ULONG64 exp = 0;
+	bool fullDump = false;
+	HANDLE dumpFile = INVALID_HANDLE_VALUE;
+
+	for (int i = 1; i <= argc - 1; ++i)
+	{
+		if (!(parseResult & ParseResultPID))
+		{
+			QString value;
+			if (GetArgValue(argv[i], ArgProcessID, &value))
+			{
+				parseResult |= ParseResultPID;
+				pid = value.toUInt();
+				continue;
+			}
+		}
+
+		if (!(parseResult & ParseResultTID))
+		{
+			QString value;
+			if (GetArgValue(argv[i], ArgThreadID, &value))
+			{
+				parseResult |= ParseResultTID;
+				tid = value.toUInt();
+				continue;
+			}
+		}
+
+		if (!(parseResult & ParseResultExp))
+		{
+			QString value;
+			if (GetArgValue(argv[i], ArgExceptionPointer, &value))
+			{
+				parseResult |= ParseResultExp;
+				exp = value.toULongLong();
+				continue;
+			}
+		}
+
+		if (!(parseResult & ParseResultFullDump))
+		{
+			QString value;
+			if (GetArgValue(argv[i], ArgFullDump, &value))
+			{
+				parseResult |= ParseResultFullDump;
+				fullDump = true;
+				continue;
+			}
+		}
+	}
+
+	if ((parseResult & (ParseResultPID | ParseResultTID | ParseResultExp)) == 
+		(ParseResultPID | ParseResultTID | ParseResultExp))
+	{
+		outDumpeeArg->PID = pid;
+		outDumpeeArg->TID = tid;
+		outDumpeeArg->Exp = exp;
+		outDumpeeArg->FullDump = fullDump;
+		outDumpeeArg->DumpFile = dumpFile;
+
+		return true;
+	}
+
+	return false;
+}
+
+typedef NTSTATUS (__stdcall *pNtQueryInformationProcess)(
+	HANDLE           ProcessHandle,
+	PROCESSINFOCLASS ProcessInformationClass,
+	PVOID            ProcessInformation,
+	ULONG            ProcessInformationLength,
+	PULONG           ReturnLength
+	);
+
+// peb->ProcessParameters->CommandLine
+QString GetProcessCommandLine(HANDLE process)
+{
+	QLibrary ntdll("Ntdll");
+	ntdll.load();
+	pNtQueryInformationProcess queryInfoProce = 
+		reinterpret_cast<pNtQueryInformationProcess>(
+		ntdll.resolve("NtQueryInformationProcess"));
+
+	if (queryInfoProce == NULL)
+	{
+		return QString();
+	}
+
+	PROCESS_BASIC_INFORMATION info = {};
+	ULONG queryLen = 0;
+	if (STATUS_SUCCESS != queryInfoProce(
+		process, ProcessBasicInformation, &info, sizeof(info), &queryLen))
+	{
+		return QString();
+	}
+
+	if (info.PebBaseAddress == NULL)
+	{
+		return QString();
+	}
+	PEB peb = {};
+	SIZE_T pebReaded = 0;
+	if (TRUE != ::ReadProcessMemory(
+		process, info.PebBaseAddress, &peb, sizeof(peb), &pebReaded))
+	{
+		return QString();
+	}
+
+	if (peb.ProcessParameters == NULL)
+	{
+		return QString();
+	}
+	RTL_USER_PROCESS_PARAMETERS param = {};
+	SIZE_T paramReaded = 0;
+	if (TRUE != ::ReadProcessMemory(
+		process, peb.ProcessParameters, &param, sizeof(param), &paramReaded))
+	{
+		return QString();
+	}
+
+	if (param.CommandLine.Buffer == NULL || param.CommandLine.Length == 0)
+	{
+		return QString();
+	}
+	// param.CommandLine.Length
+	// Specifies the length, in bytes, of the string pointed to by the Buffer member, not including the terminating NULL character, if any.
+	const size_t cmdLineLen = param.CommandLine.Length / 2 + 1;
+	std::unique_ptr<wchar_t[]> cmdLine(new wchar_t[cmdLineLen]);
+	memset(cmdLine.get(), 0, cmdLineLen * sizeof(cmdLine.get()[0]));
+	SIZE_T cmdLineReaded = 0;
+	if (TRUE != ::ReadProcessMemory(
+		process, param.CommandLine.Buffer, cmdLine.get(), param.CommandLine.Length, 
+		&cmdLineReaded))
+	{
+		return QString();
+	}
+
+	return QString::fromWCharArray(cmdLine.get());
+}
+
+typedef int (__stdcall *pQueryFeature)(int);
+
+bool NeedMailFeedback()
+{
+	QLibrary kso("kso");
+	pQueryFeature query = 
+		reinterpret_cast<pQueryFeature>(kso.resolve("_kso_QueryFeatureState"));
+
+	if (query)
+	{
+		return query(kaf_kso_MailFeedback) != 0;
+	}
+
+	return false;
+}
+
+QString GenerateCommandLine(HANDLE process, const QString& dir)
+{
+	QString cmdLine = GetProcessCommandLine(process);
+	
+	int crashCount = 0;
+	if (!cmdLine.isEmpty())
+	{
+		QString mark("recover");
+		int index = cmdLine.indexOf(mark);
+		if (index >= 0)
+			crashCount = cmdLine.mid(index + mark.length(), 1).toInt();
+	}
+
+	QString transErrorCmdLine;
+	if (NeedMailFeedback())
+	{
+		transErrorCmdLine = QString(
+			"-app=\"%1\" -dir=\"%2\" -ccrash=\"%3\" -mailback=\"%4\"")
+			.arg(GetProcessName(process)).arg(dir).arg(crashCount).arg(1);
+	}
+	else
+	{
+		transErrorCmdLine = QString("-app=\"%1\" -dir=\"%2\" -ccrash=\"%3\"")
+			.arg(GetProcessName(process)).arg(dir).arg(crashCount);
+	}
+
+	return transErrorCmdLine;
+}
+
+void SendErrorReport(const QString& cmdLine)
+{
+	QLibrary kso("kso");
+	pQueryFeature query = 
+		reinterpret_cast<pQueryFeature>(kso.resolve("_kso_QueryFeatureState"));
+
+	if (query)
+	{
+		if (query(kaf_kso_NoSendErrorReport))
+		{
+			return ;
+		}
+	}
+
+	::ShellExecuteW(NULL, NULL, L"transerr.exe", 
+		cmdLine.toStdWString().c_str(), NULL, SW_SHOWNORMAL);
+}
+
+bool CopyDirectory(const QString& srcPath, const QString& dstPath)
+{
+	if (QDir(srcPath) == QDir(dstPath))
+	{
+		return true;
+	}
+
+	QDir parentDstDir(QFileInfo(dstPath).path());
+	if (!parentDstDir.mkpath(QFileInfo(dstPath).fileName()))
+	{
+		return false;
+	}
+
+	QDir srcDir(srcPath);
+	foreach(
+		const QFileInfo &info, 
+		srcDir.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot)) 
+	{
+		QString srcItemPath = srcPath + QDir::separator() + info.fileName();
+		QString dstItemPath = dstPath + QDir::separator() + info.fileName();
+		if (info.isDir()) 
+		{
+			if (!CopyDirectory(srcItemPath, dstItemPath)) 
+			{
+				return false;
+			}
+		} 
+		else if (info.isFile()) 
+		{
+			if (QFile::exists(dstItemPath))
+			{
+				QFile::remove(dstItemPath);
+			}
+			if (!QFile::copy(srcItemPath, dstItemPath)) 
+			{
+				return false;
+			}
+		} 
+	}
+	return true;
+}
+
+void CopyLogToTempGUIDFolder(const QString& destFolder, const QString& appBaseName)
+{
+	QString tmpPath = QDir::toNativeSeparators(QDir::tempPath());
+	QString logPath= QString("%1\\log_%2").arg(tmpPath).arg(appBaseName);
+
+	QString toDir = QString("%1\\%2\\log_%3")
+		.arg(tmpPath)
+		.arg(appBaseName)
+		.arg(appBaseName);
+
+	CopyDirectory(logPath, toDir);
+}
+}
+
 int Dumper::dump(const DumpeeArg& arg)
 {
 	DumpeeArg dumpeeArg = arg;
 
 	std::shared_ptr<void> process( 
 		::OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 
-			FALSE, dumpeeArg.PID), ::CloseHandle);
+		FALSE, dumpeeArg.PID), ::CloseHandle);
 	if (!process)
 	{
 		return -1;
 	}
 
-	QString processName = QFileInfo(getProcessName(process.get())).baseName();
-	QString dumpDir = createDumpDir();
+	QString processName = QFileInfo(GetProcessName(process.get())).baseName();
+	QString dumpDir = CreateDumpDir();
 	if (dumpeeArg.DumpFile == INVALID_HANDLE_VALUE)
 	{
-		dumpeeArg.DumpFile = createDumpFile(processName, dumpDir);
+		dumpeeArg.DumpFile = CreateDumpFile(processName, dumpDir);
 	}
-	
+
 	MINIDUMP_EXCEPTION_INFORMATION info = {};
 	info.ClientPointers = TRUE;
 	info.ThreadId = dumpeeArg.TID;
@@ -210,7 +508,7 @@ int Dumper::dump(const DumpeeArg& arg)
 	if (pMiniDumpWriteDump != NULL)
 	{
 		MINIDUMP_CALLBACK_INFORMATION callbackInfo = {0};
-		callbackInfo.CallbackRoutine = miniDumpCallback;
+		callbackInfo.CallbackRoutine = MiniDumpCallback;
 		callbackInfo.CallbackParam = modules.get();
 
 		dumpSuccess = pMiniDumpWriteDump(
@@ -230,113 +528,18 @@ int Dumper::dump(const DumpeeArg& arg)
 
 	if (dumpSuccess)
 	{
-		createReportXml(
+		CreateReportXml(
 			process.get(), reinterpret_cast<PEXCEPTION_POINTERS>(dumpeeArg.Exp), 
 			modules.get(), dumpDir);
+
+		CopyLogToTempGUIDFolder(dumpDir, processName);
+
+		SendErrorReport(GenerateCommandLine(process.get(), dumpDir));
 
 		return 1;
 	}
 
 	return -2;
-}
-
-bool Dumper::getArgValue(const char* arg, const char* argKey, QString* outArgValue)
-{
-	if (arg == nullptr || argKey == nullptr || outArgValue == nullptr)
-	{
-		return false;
-	}
-
-	QString s(arg);
-	auto pos = s.indexOf(argKey);
-	if (pos != -1)
-	{
-		*outArgValue = s.mid(pos + QString(argKey).length());
-		return true;
-	}
-
-	return false;
-}
-
-bool Dumper::parseDumpeeArg(int argc, char* argv[], DumpeeArg* outDumpeeArg)
-{
-	if (argc < MandatoryArgCount)
-	{
-		return false;
-	}
-
-	if (outDumpeeArg == nullptr)
-	{
-		return false;
-	}
-
-	int parseResult = 0;
-	DWORD pid = -1;
-	DWORD tid = -1;
-	ULONG64 exp = 0;
-	bool fullDump = false;
-	HANDLE dumpFile = INVALID_HANDLE_VALUE;
-
-	for (int i = 1; i <= argc - 1; ++i)
-	{
-		if (!(parseResult & ParseResultPID))
-		{
-			QString value;
-			if (getArgValue(argv[i], ArgProcessID, &value))
-			{
-				parseResult |= ParseResultPID;
-				pid = value.toUInt();
-				continue;
-			}
-		}
-
-		if (!(parseResult & ParseResultTID))
-		{
-			QString value;
-			if (getArgValue(argv[i], ArgThreadID, &value))
-			{
-				parseResult |= ParseResultTID;
-				tid = value.toUInt();
-				continue;
-			}
-		}
-
-		if (!(parseResult & ParseResultExp))
-		{
-			QString value;
-			if (getArgValue(argv[i], ArgExceptionPointer, &value))
-			{
-				parseResult |= ParseResultExp;
-				exp = value.toULongLong();
-				continue;
-			}
-		}
-
-		if (!(parseResult & ParseResultFullDump))
-		{
-			QString value;
-			if (getArgValue(argv[i], ArgFullDump, &value))
-			{
-				parseResult |= ParseResultFullDump;
-				fullDump = true;
-				continue;
-			}
-		}
-	}
-
-	if ((parseResult & (ParseResultPID | ParseResultTID | ParseResultExp)) == 
-		(ParseResultPID | ParseResultTID | ParseResultExp))
-	{
-		outDumpeeArg->PID = pid;
-		outDumpeeArg->TID = tid;
-		outDumpeeArg->Exp = exp;
-		outDumpeeArg->FullDump = fullDump;
-		outDumpeeArg->DumpFile = dumpFile;
-
-		return true;
-	}
-
-	return false;
 }
 
 /*
@@ -359,7 +562,7 @@ int main(int argc, char* argv[])
 	if (argc >= MandatoryArgCount)
 	{
 		DumpeeArg arg = {};
-		if (Dumper::parseDumpeeArg(argc, argv, &arg))
+		if (ParseDumpeeArg(argc, argv, &arg))
 		{
 			int ret = Dumper::dump(arg);
 			return ret;
--- out_of_process_dump/dumper.h
+++ out_of_process_dump/dumper.h
@@ -1,9 +1,7 @@
 ﻿#ifndef _DUMPER_H_
 #define _DUMPER_H_
 
-#include <QString>
 #include <windows.h>
-#include <dbghelp/dbghelp.h>
 
 struct DumpeeArg
 {
@@ -17,19 +15,7 @@ struct DumpeeArg
 class Dumper
 {
 public:
-	static BOOL WINAPI miniDumpCallback(
-		__inout PVOID CallbackParam,
-		__in    PMINIDUMP_CALLBACK_INPUT CallbackInput,
-		__inout PMINIDUMP_CALLBACK_OUTPUT CallbackOutput
-		);
-	static QString createDumpDir();
-	static QString getProcessName(HANDLE process);
-	static HANDLE createDumpFile(
-		const QString& processName, const QString& dumpDir);
 	static int dump(const DumpeeArg& arg);
-	static bool getArgValue(
-		const char* arg, const char* argKey, QString* outArgValue);
-	static bool parseDumpeeArg(int argc, char* argv[], DumpeeArg* outDumpeeArg);
 };
 
 #endif
